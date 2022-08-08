05fd7583631e59b4d0af75dd970c9f4dc8bf6fcf
Ayan gupta <78692566+Ayan-thecodeking@users.noreply.github.com>
2022-08-08 13:02:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const adviceText = document.querySelector(".quote")
+const  AdviceId = document.querySelector("#id")
+const  quoteBtn = document.querySelector("button")
+const voiceBtn= document.getElementById("voice");
+
+var speech = new SpeechSynthesisUtterance();
+console.log(speech);
+
+
+
+const randomAdvice = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice")
+  const result = await res.json()
+  console.log(result)
+  adviceText.innerText = result.slip.advice
+  AdviceId.innerText = "Advice #" + `${result.slip.id}`
+  const text = result.slip.advice;
+
+  if (text.trim() != "") {
+    speech.text = text;
+    speech.rate = 0.9;
+    speech.pitch = 1;
+    speech.lang = "en-US";
+    speechSynthesis.speak(speech);
+    speech.onend=()=>{
+        alert("ended")
+    }
+
+  } else {
+    alert("Unable To Read Please Try Again Later");
+  }
+}
+
+
+
+if ("speechSynthesis" in window) {
+  // Speech Synthesis supported 🎉
+} else {
+  // Speech Synthesis Not Supported 😣
+  alert("Sorry, your browser doesn't support text to speech!");
+}
+
+function TexttoSpeech() {
+  const text = adviceText.innerText;
+  if (text.trim() != "") {
+    speech.text = text;
+    speech.rate = 0.9;
+    speech.pitch = 1;
+    speech.lang = "en-US";
+    speechSynthesis.speak(speech);
+  } else {
+    alert("Please Enter Text");
+  }
+}
+
+voiceBtn.addEventListener("click", () => {
+  TexttoSpeech();
+});
+
+quoteBtn.addEventListener("click", randomAdvice)
+
